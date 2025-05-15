3fb5655b507b9a459e74a86ed86ed44fea4dc59b
Z125062-MateuszPawel-Prowans <z125062@shibaura-it.ac.jp>
2025-05-15 10:23:40 +0900

    part1

---
+++ part1/main.js
@@ -0,0 +1,26 @@
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const newQuoteBtn = document.getElementById("newQuoteBtn");
+
+var quotes = [];
+
+function getQuote() {
+    if(quotes.length === 0) {
+        fetch("/part1/quotes.json")
+            .then(response => response.json())
+            .then(data => {
+                quotes = data;
+                getQuote();
+            })
+            .catch(error => console.error("Error fetching quotes:", error));
+    } else {
+        const quote = quotes[Math.floor(Math.random() * quotes.length)];
+
+        quoteText.textContent = quote.q;
+        authorText.textContent = quote.a;
+    }
+
+
+}
+newQuoteBtn.addEventListener("click", getQuote);
+getQuote();
